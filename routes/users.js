7791d76abd3410b40f0623d9742e2bfa2c04a315
Ines CV <inescastelltort@gmail.com>
2019-03-01 14:17:40 +0100

    UPDATE get route comments to make them more understandable

--- routes/users.js
+++ routes/users.js
@@ -10,27 +10,32 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
-// Lleva al perfil del usuario (sin imagen aún), que es modificable en ese mismo instante pero tiene los datos ya puestos por si apreta accept no se queden los campos vacios
+// GETS the user profile landing page, where he can directly edit over his information (no image yet)
 router.get('/profile', (req, res) => {
   res.render('user/profile', { title: 'Profile' });
 });
 
-// Lleva a las clases que imparte el usuario (Las modelo)
+// GETS the 3 options of user class views (should pass object class but it does not exist yet)
 router.get('/classes', (req, res) => {
   res.render('user/classes', { title: 'The classes I teach' });
 });
 
-// Lleva a las clases que imparte el usuario (Las modelo)
+// GETS the model classes that the user has created (should pass object class but it does not exist yet)
+router.get('/classes/models', (req, res) => {
+  res.render('user/classes/models', { title: 'Classes to Attend' });
+});
+
+// GETS the classes that the user has booked to attend (should pass object class but it does not exist yet)
 router.get('/classes/attending', (req, res) => {
   res.render('user/classes/attending', { title: 'Classes to Attend' });
 });
 
-// Lleva a las clases que imparte el usuario (Las modelo)
+// GETS the classes that have been booked for the teacher (should pass object class but it does not exist yet)
 router.get('/classes/teaching', (req, res) => {
   res.render('user/classes/teaching', { title: 'Classes to teach' });
 });
 
-// Brings you to create a new class
+// GETS to the form to create a new class
 router.get('/classes/newclass', (req, res) => {
   res.render('user/classes/newclass', { title: 'Create a new class' });
 });
