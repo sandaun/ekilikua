1ee5838f2e231e1f9569af5a4eac7e3ea7261282
Oriol Carbo <oriol.carbo@gmail.com>
2019-03-02 15:02:37 +0100

    ADDS Class routes

--- routes/classes.js
+++ routes/classes.js
@@ -1,76 +1,27 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const assets = require('../assets');
-const User = require('../models/user');
+const Class = require('../models/class');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Ekilikua', copyright: '© 2019 Ekilikua' });
+router.get('/', (req, res, next) => {
+  Class.find()
+    .then((element) => {
+      res.render('index', { element });
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-router.get('/login', assets.anonRoute, (req, res) => {
-  res.render('auth/login', { title: 'Log In' });
-});
-
-router.post('/login', assets.anonRoute, async (req, res, next) => {
-  const { password, email } = req.body;
-
-  if (password === '' || email === '') {
-    req.flash('error', 'Email or password empty.');
-    console.log('Email or password empty.');
-    return res.redirect('/login');
-  }
-
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const userFound = await User.findOne({ email });
-    if (!userFound) {
-      console.log("User doesn't exists");
-      return res.redirect('/signup');
-    }
-    if (bcrypt.compareSync(password, userFound.password)) {
-      req.session.currentUser = userFound;
-      return res.redirect('/success');
-    }
-    console.log('Email or password incorrect.');
-    return res.redirect('/login');
+    const classes = await Class.findById(id); // Name to be changed but class is reserved
+    res.render('index/show', { classes }); // Render to be defined
   } catch (error) {
     next(error);
   }
-});
-
-router.get('/signup', assets.anonRoute, (req, res) => {
-  res.render('auth/signup', { title: 'Sign Up' });
-});
-
-router.post('/signup', assets.anonRoute, async (req, res, next) => {
-  const { username, password, email } = req.body;
-
-  if (username === '' || password === '' || email === '') {
-    req.flash('error', 'No empty fields allowed.');
-    console.log('No empty filds allowed.');
-    return res.redirect('/signup');
-  }
-
-  try {
-    const userFound = await User.findOne({ email });
-    if (userFound) {
-      req.flash('error', `${email} already exists`);
-      console.log(`${email} already exists`);
-      return res.redirect('/signup');
-    }
-    const hashedpassword = bcrypt.hashSync(password, 10);
-    await User.create({ username, email, password: hashedpassword });
-    req.flash('success', 'User succesfully created.');
-    return res.redirect('/');
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get('/success', (req, res) => {
-  res.render('success', { title: 'Success' });
 });
 
 module.exports = router;
