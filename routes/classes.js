111b98b4ecfc8d7cc11a9a4e9dac76000312dc65
Oriol Carbo <oriol.carbo@gmail.com>
2019-03-04 22:17:34 +0100

    ADDS classes list and classcard

--- routes/classes.js
+++ routes/classes.js
@@ -4,21 +4,33 @@ const Class = require('../models/class');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  Class.find()
-    .then((element) => {
-      res.render('index', { element });
-    })
-    .catch((error) => {
-      next(error);
-    });
+// router.get('/', (req, res, next) => {
+//   Class.find()
+//     .then((element) => {
+//       res.render('index', { element });
+//     })
+//     .catch((error) => {
+//       next(error);
+//     });
+// });
+
+// List of all available classes
+router.get('/', async (req, res) => {
+  try {
+    const availableClasses = await Class.find({});
+    console.log('All the available classes ', availableClasses);
+    res.render('classes/index', { availableClasses, title: 'Choose your class' });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/:id', async (req, res, next) => {
   const { id } = req.params;
+  console.log(id);
   try {
-    const classes = await Class.findById(id); // Name to be changed but class is reserved
-    res.render('index/show', { classes }); // Render to be defined
+    const selectedClass = await Class.findById(id);
+    res.render('classes/classcard', { selectedClass, title: selectedClass.title });
   } catch (error) {
     next(error);
   }
