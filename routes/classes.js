b0e988fd5f3affbcf1ddf9b52479e49e104bcf91
JdeJ <jorgedjuana@gmail.com>
2019-03-06 17:30:50 +0100

    ADDS classcard view control to show join/leave class

--- routes/classes.js
+++ routes/classes.js
@@ -14,11 +14,34 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+// View one class detail
 router.get('/:classID', async (req, res, next) => {
   const { classID } = req.params;
   try {
-    const selectedClass = await Class.findById(classID).populate('profesor alumns');
+    const selectedClass = await Class.findById(classID).populate('professor alumns');
     res.render('classes/classcard', { lesson: selectedClass, view: 'all' });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Join one class
+router.get('/:classID/join', async (req, res, next) => {
+  const { classID } = req.params;
+  try {
+    await Class.findByIdAndUpdate(classID, { $push: { alumns: [res.locals.currentUser._id] } }, { new: true });
+    res.redirect(`/classes/${classID}`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Leave one class
+router.get('/:classID/leave', async (req, res, next) => {
+  const { classID } = req.params;
+  try {
+    await Class.findByIdAndUpdate(classID, { $pullAll: { alumns: [res.locals.currentUser._id] } }, { new: true });
+    res.redirect(`/classes/${classID}`);
   } catch (error) {
     next(error);
   }
