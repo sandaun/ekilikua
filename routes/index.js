cd12d6dd33484fd42d388488ea76bab997e7ba62
Oriol Carbo <oriol.carbo@gmail.com>
2019-03-06 21:28:14 +0100

    ADDS miniclasscard partial to classlist and index views. Also modified index routes file.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const Class = require('../models/class');
 
 const router = express.Router();
 
 // Renders homepage
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Ekilikua', copyright: '© 2019 Ekilikua' });
+router.get('/', async (req, res, next) => {
+  try {
+    const classes = await Class.find();
+    console.log('All the available classes ', classes);
+    res.render('index',
+      {
+        title: 'Ekilikua',
+        copyright: '© 2019 Ekilikua',
+        classes,
+        view: 'all',
+      });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
