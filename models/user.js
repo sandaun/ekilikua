755f3e11859230505f26be550a548552bf2fefd6
JdeJ <jorgedjuana@gmail.com>
2019-03-07 01:09:38 +0100

    UPDATES user model and user profile, to add kuas

--- models/user.js
+++ models/user.js
@@ -10,15 +10,20 @@ const userSchema = new Schema({
   // email: { type: String },
   // password: { type: String },
 
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: true },
+  password: { type: String, required: true},
   description: String,
   avatar: {
     type: String,
     default: 'http://illustrationfriday.com/wp-content/uploads/2018/09/food-460x650.png',
   },
   classes: [{ type: ObjectId, ref: 'Class' }],
+  kuas: {
+    type: Number,
+    min: 0,
+    default: 200,
+  },
 }, {
   timestamps: true,
 });
