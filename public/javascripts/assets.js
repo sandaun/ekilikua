18f0136dea2c7e69b7938458c923b9c1a1b32666
JdeJ <jorgedjuana@gmail.com>
2019-03-10 18:41:37 +0100

    ADDS variables.scss with global variables, colors....

--- public/javascripts/assets.js
+++ public/javascripts/assets.js
@@ -26,18 +26,3 @@ function setMinScheduleDate() {
   today = `${yyyy}-${mm}-${dd}`;
   document.getElementById('schedule').setAttribute('min', today);
 }
-
-function convertDateToString(date) {
-  let dd = date.getDate();
-  let mm = date.getMonth() + 1;
-  let yyyy = date.getFullYear();
-  if (dd < 10) {
-    dd = `0${dd}`;
-  }
-  if (mm < 10) {
-    mm = `0${mm}`;
-  }
-  const converted = `${yyyy}-${mm}-${dd}`;
-  console.log(converted);
-  document.getElementById('schedule').setAttribute('value', converted);
-}
