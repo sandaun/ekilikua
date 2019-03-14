5cd603b85d08ea019bd68bb7d8bd408921cf2000
JdeJ <jorgedjuana@gmail.com>
2019-03-14 17:16:48 +0100

    UPDATES auth routes with flash notifications

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,6 @@ router.post('/', assets.anonRoute, async (req, res, next) => {
 
   if (password === '' || email === '') {
     req.flash('error', 'Email or password empty.');
-    console.log('Email or password empty.');
     return res.redirect('/auth');
   }
 
@@ -24,7 +23,6 @@ router.post('/', assets.anonRoute, async (req, res, next) => {
     const userFound = await User.findOne({ email });
     if (!userFound) {
       req.flash('error', "User doesn't exist.");
-      console.log("User doesn't exists");
       return res.redirect('/auth/signup');
     }
     if (bcrypt.compareSync(password, userFound.password)) {
@@ -34,7 +32,6 @@ router.post('/', assets.anonRoute, async (req, res, next) => {
       delete req.session.returnTo;
     }
     req.flash('error', 'Email or password incorrect.');
-    console.log('Email or password incorrect.');
     return res.redirect('/auth');
   } catch (error) {
     next(error);
@@ -52,20 +49,18 @@ router.post('/signup', assets.anonRoute, async (req, res, next) => {
 
   if (name === '' || password === '' || email === '' || description === '') {
     req.flash('error', 'No empty fields allowed.');
-    console.log('No empty filds allowed.');
     return res.redirect('/signup');
   }
 
   try {
     const userFound = await User.findOne({ email });
     if (userFound) {
-      req.flash('error', `${email} already exists`);
-      console.log(`${email} already exists`);
+      req.flash('error', `Sorry, ${email} already exists`);
       return res.redirect('/signup');
     }
     const hashedpassword = bcrypt.hashSync(password, 10);
     await User.create({ name, email, description, password: hashedpassword });
-    req.flash('success', 'User succesfully created.');
+    req.flash('success', `User ${name} succesfully created.`);
     return res.redirect('/auth');
   } catch (error) {
     next(error);
