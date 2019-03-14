f287963f802ac302decd697733fc8e18acc65c4b
JdeJ <jorgedjuana@gmail.com>
2019-03-14 17:24:03 +0100

    UPDATES class routes with flas notifications

--- routes/classes.js
+++ routes/classes.js
@@ -4,14 +4,12 @@ const Class = require('../models/class');
 const User = require('../models/user');
 const assets = require('../assets');
 
-
 const router = express.Router();
 
 // List of all available classes
 router.get('/', async (req, res, next) => {
   try {
     const classes = await Class.find();
-    console.log('All the available classes ', classes);
     res.render('classes/classlist', { classes, view: 'all' });
   } catch (error) {
     next(error);
@@ -27,10 +25,10 @@ router.get('/query', async (req, res, next) => {
                                            { 'categoryID' : { $regex: new RegExp(query, "i") }}, 
                                            { 'level' : { $regex: new RegExp(query, "i") }}, 
                                            { 'description' : { $regex: new RegExp(query, "i") }}]});
-    console.log('Query classes ', classes);
 
     // If 0 coincidences, suggested class
     if (classes.length === 0) {
+      req.flash('info', 'No matches found.');
       found = false;
       const random = Math.floor(Math.random() * await Class.count());
       classes = await Class.findOne().skip(random);
@@ -62,7 +60,7 @@ router.get('/:classID/join', assets.authRoute, async (req, res, next) => {
     let lesson = await Class.findById(classID).populate('professor alumns');
 
     if (user.name === lesson.professor.name) {
-      console.log("Proffesor can't join his own class.");
+      req.flash('error', "Proffesor can't join his own class.");
     } else {
       let booked = false;
       let kuas;
@@ -101,6 +99,7 @@ router.get('/:classID/join', assets.authRoute, async (req, res, next) => {
         }
       }
     }
+    req.flash('success', 'Congratulations you will learn new knowledge.');
     res.redirect(`/classes/${classID}`);
   } catch (error) {
     next(error);
@@ -141,6 +140,7 @@ router.get('/:classID/leave', assets.authRoute, async (req, res, next) => {
         await User.findByIdAndUpdate(alumn.id, { kuas: paid }, { new: true });
       });
     }
+    req.flash('info', 'You will loose this new knowledge.');
     res.redirect(`/classes/${classID}`);
   } catch (error) {
     next(error);
