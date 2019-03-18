65bc0b65b89172cf7b9a0ccb2361ce0dc39b3c4c
JdeJ <jorgedjuana@gmail.com>
2019-03-18 19:34:06 +0100

    ADDS mapbox support

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,33 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   try {
     const classes = await Class.find();
-    res.render('index', { classes, view: 'all' });
+    let points = {
+      type: 'FeatureCollection',
+      features: [{
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [2.188854217529297, 41.43085452425],
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Java',
+        },
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [2.1865206956863403, 41.40263407490894],
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Marketing',
+        },
+      }],
+    };
+
+    res.render('index', { classes, points, view: 'all' });
   } catch (error) {
     next(error);
   }
