c2b94bb272351e6cf7f51bdfb4759232178183dc
Ines CV <inescastelltort@gmail.com>
2019-05-02 19:54:08 +0200

    FIX typo

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const { ObjectId } = Schema.Types;
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true},
+  password: { type: String, required: true },
   description: String,
   avatar: {
     type: String,
